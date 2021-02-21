6d9368aada975c369d3ba3845708c05e7f705ef1
Fareez <60868965+iqfareez@users.noreply.github.com>
2021-02-21 11:30:45 +0800

    Final release (#3)

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ A simple Flutter app showing current light intensity.
 
 ## Dependecies
 
-[light 1.0.0](https://pub.dev/packages/light)
+| Name                                                      |
+| --------------------------------------------------------- |
+| [light](https://pub.dev/packages/light)                   |
+| [url_launcher](https://pub.dev/packages/url_launcher)     |
+| [auto_size_text](https://pub.dev/packages/auto_size_text) |
 
 ## Download the app
 
-[For Android](https://play.google.com/store/apps/details?id=com.fareez.lightLux)
+<a href='https://play.google.com/store/apps/details?id=com.fareez.lightLux&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' width="200"/></a>
--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:light/light.dart';
-import 'package:light_lux/equivalent.dart';
+import 'equivalent.dart';
 
 class AppPage extends StatefulWidget {
   @override
@@ -12,21 +13,19 @@ class _AppPageState extends State<AppPage> {
   String _luxString = 'Unknown';
   String _equivalentText = 'Unknown';
   Light _light;
-  Color bgColor = Colors.white;
+  double _opacity;
   StreamSubscription _subscription;
 
   void onData(int luxValue) async {
-    // print("Lux value: $luxValue");
     setState(() {
       _luxString = "$luxValue";
-      bgColor = Colors.blue.withAlpha(luxValue.clamp(0, 255));
+      _opacity = newValueFromRange(
+          luxValue.clamp(0, 800).toDouble(), 0, 800, 0.1, 1.0);
       _equivalentText = EquivalentLuxValue.getEquivalent(luxValue);
     });
   }
 
-  void stopListening() {
-    _subscription.cancel();
-  }
+  void stopListening() => _subscription.cancel();
 
   void startListening() {
     _light = Light();
@@ -41,44 +40,50 @@ class _AppPageState extends State<AppPage> {
   void initState() {
     super.initState();
     initPlatformState();
+    _opacity = 0.0;
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    startListening();
-  }
+  Future<void> initPlatformState() async => startListening();
+
+  double newValueFromRange(
+          num value, num oldMin, num oldMax, num newMin, num newMax) =>
+      (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: bgColor,
-      child: Stack(
-        children: [
-          Center(
-            child: Text(
-              _luxString,
-              style: TextStyle(
-                fontSize: 173,
-                fontWeight: FontWeight.w100,
-              ),
-              textAlign: TextAlign.center,
-            ),
+    return Stack(
+      children: [
+        AnimatedOpacity(
+          opacity: _opacity,
+          duration: Duration(milliseconds: 800),
+          child: Container(
+            color: Colors.orange,
           ),
-          Positioned(
-            width: MediaQuery.of(context).size.width,
-            bottom: 150,
-            child: Text(
-              _equivalentText,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
+        ),
+        Center(
+          child: AutoSizeText(
+            _luxString,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: 173,
+              fontWeight: FontWeight.w100,
             ),
+            textAlign: TextAlign.center,
           ),
-        ],
-      ),
+        ),
+        Positioned(
+          width: MediaQuery.of(context).size.width,
+          bottom: MediaQuery.of(context).orientation == Orientation.landscape
+              ? 50
+              : 110,
+          child: Text(
+            _equivalentText,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
     );
   }
 }
-
-// From anywhere in your app, add the following line of code:
-// Get.changeThemeMode(newThemeMode);
-// https://medium.com/swlh/flutter-dynamic-themes-in-3-lines-c3b375f292e3
