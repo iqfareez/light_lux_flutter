335434053435872665fe4404e067c9474fa3956f
iqfareez <iqmal3@outlook.com>
2022-08-07 18:28:57 +0800

    :pencil2: Minor edit on lux equivalent

--- lib/equivalent.dart
+++ lib/equivalent.dart
@@ -9,13 +9,13 @@ class EquivalentLuxValue {
     } else if (luxValue >= 1000) {
       return 'Overcast day';
     } else if (luxValue >= 400) {
-      return 'Clear day';
+      return 'Office Lighting';
     } else if (luxValue >= 320) {
       return 'Office lighting';
     } else if (luxValue >= 150) {
       return 'Train Station';
     } else if (luxValue >= 100) {
-      return 'Dark overcast day';
+      return 'Very dark overcast day';
     } else if (luxValue >= 80) {
       return 'Office building hallway';
     } else if (luxValue >= 50) {
